132dad9db88089100de4822978ec54d84a0cfcce
jensenr30 <jensenr30@gmail.com>
2014-06-17 21:18:55 -0500

    added some stuff TODO in RPRNG.h

--- RPRNG/RPRNG.h
+++ RPRNG/RPRNG.h
@@ -7,7 +7,9 @@
 struct RPRNG {
 	
 	
-	//
+	//TODO: write a random sequence generator.
+	
+	//TODO: write a genetic algorithm to generate  evaluate effectiveness of state machines, and then breed (and mutate) the most effective state machines)
 	
 	
 	
