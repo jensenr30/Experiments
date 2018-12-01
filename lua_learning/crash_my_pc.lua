ea7fbecdccfa2426c5167422b259533caa94f982
ryan <jensenr30@gmail.com>
2018-11-30 19:01:34 -0800

    lazy

---
+++ lua_learning/crash_my_pc.lua
@@ -0,0 +1,4 @@
+-- this program will run recursively and explode exponentially, consuming all the computer's resources.  Your computer may crash.  Run this program at your own risk.
+
+while true do os.execute('start cmd /K "lua crash_my_pc.lua"') end
+
---
+++ lua_learning/lua learning.lua
@@ -0,0 +1,14 @@
+-- hello world
+
+-- factorial function
+function fact (n)
+    if (n == 0) then
+        return 1
+    else
+        return n * fact(n-1)
+    end
+end
+
+print("enter a number:")
+a = io.read("*number")                                      -- ask the user for a number
+print(fact(a))
